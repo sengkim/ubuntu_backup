7aa2528f53144c5d5e3de0f7398ac5d9bbc91875
Joiky <joiky@163.com>
2011-03-15 12:44:17 +0800

    修改install_rigth_terminal.sh，增加一些安装的提示信息。

--- script/install_right_terminal.sh
+++ script/install_right_terminal.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # 右键菜单中显示终端选项，用于右键打开终端
+echo "[!!!] install nautilus-open-terminal..."
 sudo apt-get install nautilus-open-terminal
 
 (( $? != 0 )) && {
-    local target=gnome-terminal
+    target=gnome-terminal
+    echo "[!!!] install nautilus-open-terminal failed!"
+    echo "[!!!] write .gnome2/nautilus-scripts/$target"
     cat "gnome-terminal" >> $HOME/.gnome2/nautilus-scripts/$target
     chmod +x $target
+    unset target
 }
 
