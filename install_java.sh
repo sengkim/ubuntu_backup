17d12b73b8f378bb88f0215684b32cb77ea9fb74
Joiky <joiky@163.com>
2011-03-17 09:57:54 +0800

    修正install_java.sh的问题：文件检测，环境变量更新

--- install_java.sh
+++ install_java.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+default_dst_path=/usr/lib/jvm/java
+
+src_path="${1:-$PWD}"
+dst_path="${2:-$default_dst_path}"
+
 JAVA_VERSION="jdk1.6.0_24"
 
+[[ -d $src_path/$JAVA_VERSION ]] || { echo "Error: Do not find '$JAVA_VERSION' in '$src_path'"; exit 99; }
+
 sudo mkdir /usr/lib/jvm/java -p
-mv $JAVA_VERSION /usr/lib/jvm/java
+sudo mv $src_path/$JAVA_VERSION /usr/lib/jvm/java
 
-sudo echo "
-PATH=$PATH:/usr/lib/jvm/java/$JAVA_VERSION/bin
-CLASSPATH=.:/usr/lib/jvm/java/$JAVA_VERSION/lib
-JAVA_HOME=/usr/lib/jvm/java/$JAVA_VERSION" >> /etc/environment
+sudo sed '$a\
+PATH='"$PATH"':/usr/lib/jvm/java/'"$JAVA_VERSION"'/bin\nCLASSPATH=.:/usr/lib/jvm/java/'"$JAVA_VERSION"'/lib\nJAVA_HOME=/usr/lib/jvm/java/'"$JAVA_VERSION"'
+' /etc/environment -i
 
+exit 0
 . /etc/environment
 
 sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/java/$JAVA_VERSION/bin/java 300
