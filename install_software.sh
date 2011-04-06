09ab4aee901c7afd18d53e2c049c6297784bec78
Joiky <joiky.g@163.com>
2011-04-06 12:30:00 +0800

    install_software.sh中添加build-essential的安装脚本

--- install_software.sh
+++ install_software.sh
@@ -5,6 +5,9 @@ echo "install some softwares for ubuntu..."
 # 右键打开终端
 ./script/install_right_terminal.sh
 
+# build-essential
+./script/install_gcc.sh
+
 # 一个不错的终端环境，同时安排配置文件（透明）
 ./script/install_terminator.sh
 
