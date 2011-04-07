bd552ce16cb291949394255b7d34e097b4cd9625
Joiky <joiky.g@163.com>
2011-04-07 17:25:02 +0800

    修正install_java.sh脚本

--- install_java.sh
+++ install_java.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-default_dst_path=/usr/lib/jvm/java
+default_dst_path=/opt/java
 
 src_path="${1:-$PWD}"
 dst_path="${2:-$default_dst_path}"
@@ -9,17 +9,14 @@ JAVA_VERSION="jdk1.6.0_24"
 
 [[ -d $src_path/$JAVA_VERSION ]] || { echo "Error: Do not find '$JAVA_VERSION' in '$src_path'"; exit 99; }
 
-sudo mkdir /usr/lib/jvm/java -p
-sudo mv $src_path/$JAVA_VERSION /usr/lib/jvm/java
+sudo mkdir $dst_path -p
+sudo mv $src_path/$JAVA_VERSION $dst_path
 
-sudo sed '$a\
-PATH='"$PATH"':/usr/lib/jvm/java/'"$JAVA_VERSION"'/bin\nCLASSPATH=.:/usr/lib/jvm/java/'"$JAVA_VERSION"'/lib\nJAVA_HOME=/usr/lib/jvm/java/'"$JAVA_VERSION"'
-' /etc/environment -i
+sudo sed '$a\PATH=\$PATH:'"${dst_path}/$JAVA_VERSION"'/bin\nCLASSPATH=.:'"${dst_path}/$JAVA_VERSION"'/lib\nJAVA_HOME='"${dst_path}/$JAVA_VERSION"'' /etc/environment -i
 
-exit 0
 . /etc/environment
 
-sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/java/$JAVA_VERSION/bin/java 300
-sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java/$JAVA_VERSION/bin/javac 300
+sudo update-alternatives --install /usr/bin/java java ${dst_path}/$JAVA_VERSION/bin/java 300
+sudo update-alternatives --install /usr/bin/javac javac ${dst_path}/$JAVA_VERSION/bin/javac 300
 update-alternatives --config java
 
